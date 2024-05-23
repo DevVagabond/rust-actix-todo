68c4f9be7e59aa86470b9e0ab4c1563911a40c7b
Shawon Kanji <shawonkanji46@gmail.com>
2024-05-23 22:23:58 +0800

    Refactor code to use Arc and Mutex for thread-safe access to AppState

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,30 @@
+use std::sync::{Arc, Mutex};
+
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use serde_json::json;
 
+#[derive(Debug)]
 struct AppState {
     task_array: Vec<String>,
 }
 
 #[get("/api/list-task")]
-async fn list_tasks(tasks: web::Data<AppState>) -> impl Responder {
+async fn list_tasks(app: web::Data<Arc<Mutex<AppState>>>) -> impl Responder {
+    let app = app.lock().unwrap();
     let json_response = json!({
-        "tasks": &tasks.task_array
+        "tasks": app.task_array
     });
     HttpResponse::Ok().json(json_response)
 }
 
 #[post("/api/add-task")]
-async fn add_task(app: web::Data<AppState>) -> impl Responder {
+async fn add_task(app: web::Data<Arc<Mutex<AppState>>>) -> impl Responder {
+    let mut app = app.lock().unwrap();
+    app.task_array.push("Hello".to_string());
+    print!("{:#?}", app);
     HttpResponse::Ok().json(json!({
-        "tasks": []
+        "message": "Task added successfully!",
+        "tasks": app.task_array
     }))
 }
 
@@ -26,12 +34,12 @@ async fn health_check() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let task_array = web::Data::new(AppState {
+    let app_state = web::Data::new(Arc::new(Mutex::new(AppState {
         task_array: Vec::new(),
-    });
+    })));
     HttpServer::new(move || {
         App::new()
-            .app_data(task_array.clone())
+            .app_data(app_state.clone())
             .service(list_tasks)
             .service(add_task)
             .route("/health", web::get().to(health_check))
